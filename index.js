d5b37bdd8bc891b83dbbe7b0544a350ab0c4abdc
bevilla203 <ben.tan.villafranca@gmail.com>
2022-12-13 15:45:23 -0500

    Third Try!

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Hello?");
+
+/*
+1. git push should affect the repository I'm working on currently: Foundations.DOM.JSWebsite-main
+
+2. It uses this line:
+<script src="index.js"></script>
+*/
+
+// editing Math Study Cards
+const titleEle = document.head.querySelector("title");
+    titleEle.style.color = "red";
+
+console.log("Please Work");
